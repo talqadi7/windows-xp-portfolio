62b7dce1c3e2e8802a341b63b24d3976cf9d2bfb
talqadi7 <taq.alqadi@gmail.com>
2023-05-14 17:56:11 -0700

    Added resume folder

--- src/Desktop.js
+++ src/Desktop.js
@@ -7,10 +7,11 @@ import IconWrapper from './IconWrapper.js';
 function Desktop() {
   return (
     <div className="Desktop">
-        <IconWrapper>
-            <Icon name="Project 1" file="ICON/22.ico" URL="https://google.com"/>
-            <Icon name="Project 2" file="ICON/22.ico" URL="https://bing.com"/>
-        </IconWrapper>
+      <IconWrapper>
+      <Icon name="Resume" file="ICON/47.ico" URL="https://docs.google.com/document/d/1lsJXdYxkDOKdSqkepZe19U4hSm71jNho/edit?usp=sharing&ouid=103724823257916244423&rtpof=true&sd=true" />
+        <Icon name="Project 1" file="ICON/22.ico" URL="https://google.com" />
+        <Icon name="Project 2" file="ICON/22.ico" URL="https://bing.com" />
+      </IconWrapper>
 
       <Taskbar />
     </div>
